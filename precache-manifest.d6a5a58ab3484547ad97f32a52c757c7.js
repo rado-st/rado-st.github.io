8e212d6f94c8de02fee4458d872f894d5a030eff
Radoslaw Stan <radoslaw.stan96@gmail.com>
2020-05-03 20:37:49 +0200

    Updates

---
+++ precache-manifest.d6a5a58ab3484547ad97f32a52c757c7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749bfa32610ee445f5d40b3af11cae34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1592fb2b059ee3ba207",
+    "url": "/static/css/2.c9fbf016.chunk.css"
+  },
+  {
+    "revision": "b1592fb2b059ee3ba207",
+    "url": "/static/js/2.bf2d61c0.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.bf2d61c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f75bb394397d759424d3",
+    "url": "/static/js/main.ad2f6ab9.chunk.js"
+  },
+  {
+    "revision": "1b6c486bcc031584ddcb",
+    "url": "/static/js/runtime-main.3053f720.js"
+  },
+  {
+    "revision": "112020427044aab2ee527b5af25bcf94",
+    "url": "/static/media/gulvsliping.11202042.png"
+  },
+  {
+    "revision": "ad15cb9874eed4bede2e9c9710c84bca",
+    "url": "/static/media/portfolio.ad15cb98.png"
+  },
+  {
+    "revision": "39297b933679a5300407ffd606c9c19d",
+    "url": "/static/media/profile-pic.39297b93.jpg"
+  }
+]);
